4324cb390aaa46f59f311a62efce38a8e82d29b6
Ryan Rosenbaum <rosenbaum.ryan@gmail.com>
2018-11-07 19:56:16 -0800

    Did I do that???

--- frontend-urql/src/App.js
+++ frontend-urql/src/App.js
@@ -1,25 +1,66 @@
 import React, {Component} from 'react';
-import {Connect, query} from 'urql';
+import {Connect, query, mutation} from 'urql';
+// import gql from 'graphql-tag';
 
-const GET_STORE_LIST = `
-query StoreList {
-	allStores {
-		edges {
-			node {
-				id
+const GetStoreListQuery = `
+	query StoreList {
+		allUsers {
+			edges {
+				node {
+					id
+					firstName
+					lastName
+				}
+			}
+		}
+		allStores {
+			edges {
+				node {
+					id
+					name
+				}
 			}
 		}
 	}
-}
+`;
+
+const AddStoreQuery = `
+	mutation CreateStore {
+		addStore(input: {
+			ownerId: "VXNlck5vZGU6MQ=="
+			name: "Dog Shop"
+		}) {
+			clientMutationId
+			store {
+				id
+				name
+			}
+		}
+	}
 `;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Connect query={query(GET_STORE_LIST)}>
-          {({loaded, fetching, refetch, data, error, addTodo}) => {
-            return <pre>{JSON.stringify(data, null, 2)}</pre>;
+        <Connect
+          query={query(GetStoreListQuery)}
+          mutation={{
+            addStore: mutation(AddStoreQuery),
+          }}>
+          {({loaded, fetching, refetch, data, error, addStore}) => {
+            return (
+              <div>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+                <form
+                  onSubmit={e => {
+                    e.preventDefault();
+                    addStore();
+                  }}>
+                  <button type="submit">Add Store</button>
+                </form>
+              </div>
+            );
           }}
         </Connect>
       </div>
